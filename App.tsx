3c6cd618003885bb7ede49a340f7eb4b7eb506f4
Eric Rak <ericrak@Erics-Mac-mini.local>
2025-02-19 10:33:25 -0500

    Replaced old project with new one

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import useHealthData from "./hooks/useHealthData";
 
 export default function App() {
+  const { steps, distance, flights } = useHealthData();
+
+  console.log(`Steps: ${steps} | Distance: ${distance}m | Flights: ${flights}`);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Steps: {steps.toString()}</Text>
+        <Text>Distance: {(distance / 1000).toFixed(2)}</Text>
+        <Text>Flights: {flights.toString()}</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
